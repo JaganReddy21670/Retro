d5cedae3a5d7b4c4c243e51fde372ec90cf12aa2
JaganReddy21670 <jagan.reddy@research.iiit.ac.in>
2026-03-04 22:14:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ ARUCO_MARKER_IDS = [0, 1, 2, 3]
 ROI_PADDING = {
     "top": -5,    # Shrink top by 5%
     "bottom": -5, # Shrink bottom by 5%
-    "left": -20,  # Perfect balance between -10 and -18 to hide sticker but protect '4'
+    "left": -8,  # Perfect balance between -10 and -18 to hide sticker but protect '4'
     "right": -20  # Aggressively shrink to completely cut off the right marker
 }
 
